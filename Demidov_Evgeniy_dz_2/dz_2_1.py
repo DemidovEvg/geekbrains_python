bd0fcb9ed6d9495eb577740776b17648fdbf53ae
DemidovEvg <88984484+DemidovEvg@users.noreply.github.com>
2021-09-10 19:11:23 +0300

    Delete Demidov_Evgeniy_dz_2 directory

--- Demidov_Evgeniy_dz_2/dz_2_1.py
+++
@@ -1,4 +0,0 @@
-expression = '15*3', '15/3', '15//3', '15**3'
-
-for exp in expression:
-    print(f'type({exp:^5}) = {eval(exp):<5} - {type(eval(exp))}')
--- Demidov_Evgeniy_dz_2/dz_2_4.py
+++
@@ -1,7 +0,0 @@
-input_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-
-for current_el in input_list:
-    name_position = current_el.rfind(' ') + 1
-    name = current_el[name_position:len(current_el)]
-    name = name.capitalize()
-    print(f'Привет, {name}!')
