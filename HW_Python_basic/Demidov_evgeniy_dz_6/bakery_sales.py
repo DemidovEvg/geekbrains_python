47f7750c5c45d349f2d3f4a626a33c9fcb3b6e8f
Demidov Evgeniy <Demidov.evg.v@gmail.com>
2021-09-19 17:39:28 +0300

    dz_6

--- HW_Python_basic/Demidov_evgeniy_dz_6/bakery_sales.py
+++ HW_Python_basic/Demidov_evgeniy_dz_6/bakery_sales.py
@@ -25,14 +25,80 @@
 # *(вместо 6) Добавить возможность редактирования данных при помощи отдельного скрипта: передаём ему номер записи и новое значение. При этом файл не должен читаться целиком — обязательное требование. Предусмотреть ситуацию, когда пользователь вводит номер записи, которой не существует.
 
 import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Bakery sales statistic')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
 
-args = parser.parse_args()
-print(args.int111)
-#print(args.accumulate(args.integers))
+def push(path_file, data):
+    with path_file.open("a", encoding='utf-8') as f:
+        f.write(str(data) + '\n')
+
+
+def correct(path_file, data, num_row):
+    cur_line_num = 1
+    is_find_row = False
+    path_file_tmp = Path(str(path_file).replace('.csv', '') + '_tmp.csv')
+    with path_file.open("r", encoding='utf-8') as f_out: 
+        with path_file_tmp.open("a", encoding='utf-8') as f_in: 
+            line = f_out.readline()
+            while line:
+                if cur_line_num == num_row:
+                    f_in.write(str(data) + '\n')
+                    is_find_row = True
+                else:
+                    f_in.write(line)
+                cur_line_num += 1
+                line = f_out.readline()
+
+    if  not is_find_row:
+        path_file_tmp.unlink()
+        raise ValueError('File has less row than input row')
+    path_file.unlink()
+    path_file_tmp.rename(path_file)
+    
+def pull(path_file, start, end):
+    if start == None:
+        start = 1
+    elif end!= None and end < start:
+        raise ValueError('Incorrect start [or | and] end')
+    line_num = 1
+    with path_file.open("r", encoding='utf-8') as f:  
+        for line in f:
+            if line_num >= start:
+                print(line.strip())
+            line_num += 1
+            if end != None and end < line_num:
+                break
+
+
+parser = argparse.ArgumentParser(description='Sript for push and pull data for bakery sales')
+subparser = parser.add_subparsers(help='sub-command [args | -h]', required=True)
+parser_push = subparser.add_parser('push', help='[args | -h]', 
+                                    description='Sub-command for push data to file')
+parser_push.set_defaults(func=push)
+parser_push.add_argument('-c', '--correct', metavar='num_row', type=int, help='Enter num row for correct data')
+parser_push.add_argument('data', type=str, help='it is the sales in rub for save in file')
+parser_pull = subparser.add_parser('pull', help='[args | -h]',
+                                    description='Sub-command for pull data in rub from file')
+parser_pull.set_defaults(func=pull)
+parser_pull.add_argument('start', type=int, nargs='?',
+                            help='optional parameter - start row for the pull')
+parser_pull.add_argument('end', type=int, nargs='?',
+                            help='optional parameter - end row for the pull')
+
+user_input = parser.parse_args()
+
+path_file = Path('bakery.csv')
+
+if user_input.func == push:
+    try:
+        data = float(user_input.data.replace(',', '.'))
+        if user_input.correct != None:
+            correct(path_file, user_input.data, user_input.correct)
+        else:
+            push(path_file, user_input.data)
+    except ValueError as e:
+        print(e)
+        print('Incorrect data')
+elif user_input.func == pull:
+    pull(path_file, user_input.start, user_input.end)
+
